f82fdeddba0e859957b78c7dd3e1c4bc167f0fc0
polgasull <pgasulln@gmail.com>
2020-02-16 23:34:10 +0100

    Added blog post page

--- app/public/wp-content/themes/pizzashop/functions.php
+++ app/public/wp-content/themes/pizzashop/functions.php
@@ -3,6 +3,10 @@
 function pizzashop_setup() {
   add_theme_support('post-thumbnails');
 
+  /** Custom image sizes */
+  add_image_size('about_us', 437, 291, true);
+  add_image_size('specialties', 768, 515, true);
+  add_image_size('portrait_specialties', 435, 526, true);
 }
 add_action('after_setup_theme', 'pizzashop_setup');
 
@@ -24,7 +28,9 @@ function pizzashop_styles() {
   wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0');
   wp_enqueue_style('header', get_template_directory_uri().'/css/header.css', array(), '1.0.1');
   wp_enqueue_style('footer', get_template_directory_uri().'/css/footer.css', array(), '1.0.1');
-  wp_enqueue_style('menu', get_template_directory_uri().'/css/menu.css', array(), '1.0.1');
+  wp_enqueue_style('menus', get_template_directory_uri().'/css/menus.css', array(), '1.0.1');
+  wp_enqueue_style('pages', get_template_directory_uri().'/css/pages.css', array(), '1.0.1');
+  wp_enqueue_style('blog', get_template_directory_uri().'/css/blog.css', array(), '1.0.1');
   wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
   wp_enqueue_script('slicknavJS', get_template_directory_uri().'/js/jquery.slicknav.js', array('jquery'), '1.0.10', true);
   wp_enqueue_script('scripts', get_template_directory_uri().'/js/scripts.js', array('jquery'), '1.0.0', true);
---
+++ app/public/wp-content/themes/pizzashop/home.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+
+  <?php
+    $blog_page = get_option('page_for_posts');
+    $image_id = get_post_thumbnail_id($blog_page);
+    $image_src = wp_get_attachment_image_src($image_id, 'full')[0];
+  ?>
+
+  <div class="hero" style="background-image: url( <?php echo $image_src ?>)">
+    <div class="content-hero">
+      <h1><?php echo get_the_title($blog_page); ?></h1>
+    </div>
+  </div>
+
+  <div class="section container with-sidebar">
+    <main class="main-content">
+      <?php while(have_posts()): the_post(); ?>
+        <article class="blog-post">
+          <a href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('specialties'); ?>
+          </a>
+          <header class="post-info">
+            <div class="date">
+              <time>
+                <?php echo the_time('d'); ?>
+                <span><?php echo the_time('M'); ?></span>
+              </time>
+            </div>
+            <div class="post-title">
+              <h2><?php the_title(); ?></h2>
+            </div>
+          </header>
+          <p class="author">
+            Writed by:
+            <span><?php the_author(); ?> </span>
+          </p>
+          
+          <div class="post-content">
+            <?php the_excerpt(); ?>
+
+            <a class="button primary-button" href="<?php the_permalink(); ?>">
+              Read more 
+            </a>
+          </div>
+        </article>
+      <?php endwhile; ?>
+    </main>
+
+    <?php get_sidebar(); ?>
+
+  </div>
+
+<?php get_footer(); ?>
